17db5ede2aa3dc17b663ff3192daba0053f8bee1
nalinbot <nalinnagar1@gmail.com>
2021-09-29 20:35:24 -0400

    Created dataloader.py :fire:
    Function for loading MNIST dataset

---
+++ dataloader.py
@@ -0,0 +1,24 @@
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+def get_mnist_dataloaders(batch_size=128):
+    """MNIST dataloader with (32, 32) sized images."""
+    # Resize images so they are a power of 2
+    all_transforms = transforms.Compose([
+        transforms.Resize(32),
+        transforms.ToTensor()
+    ])
+    # Get train and test data
+    train_data = datasets.MNIST('../data', train=True, download=True,
+                                transform=all_transforms)
+    test_data = datasets.MNIST('../data', train=False,
+                               transform=all_transforms)
+    # Create dataloaders
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+    
+    return train_loader, test_loader
+  
+  
+  
